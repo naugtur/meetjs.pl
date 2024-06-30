ee4c410acbb883e45668709bfdea1dde621e66d2
Stanisław Synowiec <stasyn1410@gmail.com>
2024-06-30 18:34:49 +0200

    config: add social media redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/facebook',
+				destination: 'https://www.facebook.com/meetjspl',
+				permanent: true,
+			},
+			{
+				source: '/linkedin',
+				destination: 'https://www.linkedin.com/company/meetjs',
+				permanent: true,
+			},
+			{
+				source: '/x',
+				destination: 'https://x.com/meetjs',
+				permanent: true,
+			},
+			{
+				source: '/discord',
+				destination: 'https://discord.gg/UycCSpRh6j',
+				permanent: true,
+			},
+			{
+				source: '/github',
+				destination: 'https://github.com/meetjspl',
+				permanent: true,
+			},
+		];
+	},
+};
 
 export default nextConfig;
