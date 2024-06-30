db39740a70fb938ceb9459f1742a8d8cb7e9cb76
Stanisław Synowiec <stasyn1410@gmail.com>
2024-06-30 18:03:28 +0200

    style: add brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
 				'gradient-conic':
 					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
+			colors: {
+				purple: '#2b1932',
+				blue: '#219eab',
+				green: '#bcd35d',
+			},
 		},
 	},
 	plugins: [],
