7ab56f3e6cff5b981a76b0af8c3c3198216823ac
Stanisław Synowiec <stasyn1410@gmail.com>
2024-07-10 13:40:15 +0200

    type(event): add zod schema

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,19 +1,31 @@
+import { z } from 'zod';
+
 export interface Event {
-	id: string;
+	id: number;
+	date_add: number;
+	date: string;
+	time: string;
 	name: string;
-	description: string;
-	startDateTime: string;
-	endDateTime: string;
-	location: {
-		name: string;
-		cords: {
-			lat: number;
-			lon: number;
-		};
-	};
-	organizer: {
-		name?: string;
-		email?: string;
-	};
-	eventLink?: string;
+	type: string;
+	url: string;
+	rsvp: string;
+	city: string;
+	address: string;
+	serie: string;
 }
+
+const EventSchema = z.object({
+	id: z.number(),
+	date_add: z.number(),
+	date: z.string(),
+	time: z.string(),
+	name: z.string(),
+	type: z.string(),
+	url: z.string(),
+	rsvp: z.string(),
+	city: z.string(),
+	address: z.string(),
+	serie: z.string(),
+});
+
+export const EventsSchema = z.record(z.string(), EventSchema);
