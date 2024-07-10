2d7d6b425b0578f08399fdc588d4c0c0280df199
Stanisław Synowiec <stasyn1410@gmail.com>
2024-07-10 13:43:02 +0200

    type(event): infer event type from event zod schema

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,19 +1,5 @@
 import { z } from 'zod';
 
-export interface Event {
-	id: number;
-	date_add: number;
-	date: string;
-	time: string;
-	name: string;
-	type: string;
-	url: string;
-	rsvp: string;
-	city: string;
-	address: string;
-	serie: string;
-}
-
 const EventSchema = z.object({
 	id: z.number(),
 	date_add: z.number(),
@@ -29,3 +15,5 @@ const EventSchema = z.object({
 });
 
 export const EventsSchema = z.record(z.string(), EventSchema);
+
+export type EventType = z.infer<typeof EventSchema>;
