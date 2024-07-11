5bbe6b14cc7e0e140f3aee056987f0b5ef43a225
Stanisław Synowiec <stasyn1410@gmail.com>
2024-07-11 12:16:03 +0200

    chore(nav): add summit nav link

--- src/content/menuLinks.tsx
+++ src/content/menuLinks.tsx
@@ -1,4 +1,5 @@
 export const menuLinks = [
+	{ name: 'summit', href: 'https://summit.meetjs.pl', current: false },
 	{ name: 'events', href: '/#events', current: false },
 	{ name: 'about', href: '/#about', current: false },
 	{ name: 'contact', href: '#', current: false },
