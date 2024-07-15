09b06dce082ed8a511aeb35b633ccbd6fe514762
Stanisław Synowiec <stasyn1410@gmail.com>
2024-07-15 16:03:52 +0200

    chore(mdx): add mdx files support

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import Image from 'next/image';
+import dynamic from 'next/dynamic';
+
+const MDXContent = dynamic(() => import('@/content/about.mdx'));
 
 export const AboutSection = () => {
 	return (
@@ -17,20 +22,8 @@ export const AboutSection = () => {
 			</div>
 			<div className="w-full md:w-1/2">
 				<h2 className="text-center text-3xl font-bold">About meet.js</h2>
-				<div className="flex gap-4 p-4">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua.
-						Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam
-						vehicula. Orci sagittis eu volutpat odio facilisis mauris. Urna
-						neque viverra justo nec ultrices dui sapien eget mi. In hac
-						habitasse platea dictumst vestibulum rhoncus. Arcu bibendum at
-						varius vel pharetra vel. Nulla malesuada pellentesque elit eget
-						gravida cum sociis natoque penatibus. Ultricies lacus sed turpis
-						tincidunt id aliquet risus. Urna neque viverra justo nec ultrices.
-						Amet consectetur adipiscing elit duis tristique sollicitudin nibh
-						sit amet.
-					</p>
+				<div className="flex flex-col gap-4 p-4">
+					<MDXContent />
 				</div>
 			</div>
 		</section>
