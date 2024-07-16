5681433e964a5b3f0398bb33ff3c55f581c0a90a
Stanisław Synowiec <stasyn1410@gmail.com>
2024-07-16 17:57:46 +0200

    chore(past events): add past events fetch & fix event schema

--- src/types/event.ts
+++ src/types/event.ts
@@ -4,14 +4,15 @@ const EventSchema = z.object({
 	id: z.number(),
 	date_add: z.number(),
 	date: z.string(),
-	time: z.string(),
+	time: z.string().default(''),
 	name: z.string(),
 	type: z.string(),
 	url: z.string(),
 	rsvp: z.string(),
 	city: z.string(),
-	address: z.string(),
+	address: z.string().nullable(),
 	serie: z.string(),
+	topic: z.array(z.string()).default([]),
 });
 
 export const EventsSchema = z.record(z.string(), EventSchema).nullable();
