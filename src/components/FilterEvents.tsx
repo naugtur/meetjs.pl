2444e2b09b2f212575b24759c06f5638793d9c17
Stanisław Synowiec <stasyn1410@gmail.com>
2024-07-17 17:58:29 +0200

    chore(filtered events): add events list with city filter

---
+++ src/components/FilterEvents.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useQueryState } from 'nuqs';
+import { badgeVariants } from '@/components/ui/badge';
+import { CITIES } from '@/content/cities';
+import { EventCard } from '@/components/EventCard';
+import { Button } from '@/components/ui/button';
+import { EventsType } from '@/types/event';
+import { clsx } from 'clsx';
+
+interface FilterEventsProps {
+	events: EventsType;
+}
+
+export const FilterEvents = ({ events }: FilterEventsProps) => {
+	const [cityFilter, setCity] = useQueryState('city');
+
+	return (
+		<>
+			<div className="flex flex-col gap-2">
+				<p>Filter events by city:</p>
+				<div className="flex gap-2">
+					<Button
+						size="sm"
+						className={clsx(badgeVariants({ variant: 'default' }))}
+						onClick={() => setCity(null)}
+					>
+						All ({events !== null ? Object.keys(events).length : 0})
+					</Button>
+					{CITIES.map((city) => {
+						return (
+							<Button
+								size="sm"
+								className={clsx(
+									badgeVariants({
+										variant: cityFilter === city.name ? 'default' : 'outline',
+									}),
+									cityFilter === city.name ? '' : 'bg-transparent',
+								)}
+								key={city.name}
+								onClick={() => setCity(city.name)}
+							>
+								{city.name}
+							</Button>
+						);
+					})}
+				</div>
+			</div>
+
+			{events !== null && (
+				<ul className="flex flex-col gap-4">
+					{Object.keys(events).map((key) => {
+						const event = events[key];
+
+						if (cityFilter && event.city !== cityFilter) {
+							return null;
+						}
+
+						return (
+							<li key={event.id}>
+								<EventCard event={event} />
+							</li>
+						);
+					})}
+				</ul>
+			)}
+		</>
+	);
+};
