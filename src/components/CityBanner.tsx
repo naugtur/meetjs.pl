84295b4eb19664278be0778aa1c93ecfa6021227
Stanisław Synowiec <stasyn1410@gmail.com>
2024-07-30 20:26:49 +0200

    chore(city template): city baner section

---
+++ src/components/CityBanner.tsx
@@ -0,0 +1,39 @@
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+
+interface CityBannerProps {
+	city: string;
+	background: string;
+}
+
+export const CityBanner = ({ city, background }: CityBannerProps) => {
+	return (
+		<section
+			className="flex flex-col items-center justify-center bg-cover bg-center p-28"
+			style={{ backgroundImage: `url(${background})` }}
+		>
+			<h1 className="text-6xl font-bold text-white">{city}</h1>
+			<Breadcrumb className="bg-gray-400/50 p-2">
+				<BreadcrumbList>
+					<BreadcrumbItem>
+						<BreadcrumbLink href="/" className="text-white">
+							Home
+						</BreadcrumbLink>
+					</BreadcrumbItem>
+					<BreadcrumbSeparator className="text-white" />
+					<BreadcrumbItem>
+						<BreadcrumbPage className="font-bold text-white">
+							Wrocław
+						</BreadcrumbPage>
+					</BreadcrumbItem>
+				</BreadcrumbList>
+			</Breadcrumb>
+		</section>
+	);
+};
