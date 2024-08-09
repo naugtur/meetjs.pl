eb63eb4ccbf32b551d5f868986da798648bbe874
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2024-08-09 16:24:58 +0200

    feat: add Warsaw city page

---
+++ src/app/(cities)/warszawa/_Organizers.tsx
@@ -0,0 +1,18 @@
+import { Organizer } from '@/components/Orginizers';
+
+export const WarsawOrganizers: Organizer[] = [
+	{
+		name: 'Zbyszek Tenerowicz',
+		image: 'https://avatars.githubusercontent.com/u/509375?v=4',
+		email: '',
+		linkedin: '',
+		gitHub: 'https://github.com/naugtur',
+	},
+	{
+		name: 'Kamil Dzieniszewski',
+		image: 'https://avatars.githubusercontent.com/u/3521624?v=4',
+		email: 'kamil.dzieniszewski@gmail.com',
+		linkedin: 'https://www.linkedin.com/in/dzieniszewski/',
+		gitHub: 'https://github.com/dzienisz',
+	},
+];
