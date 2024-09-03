fb1f5413954ba70870930401b85eb3d37a7f3a9f
Krzysztof Krawiec <kkrawiec@sii.pl>
2024-09-03 10:16:05 +0200

    Lodz page implementation

---
+++ src/app/(cities)/lodz/_FAQ.tsx
@@ -0,0 +1,44 @@
+import { FAQQuestion } from '@/components/FAQ';
+
+export const LodzFAQ: FAQQuestion[] = [
+	{
+		id: '1',
+		question: 'How to become Łódź meetup sponsor?',
+		answer: 'Contact local Organizer.',
+	},
+	{
+		id: '2',
+		question: 'How to become Łódź speaker?',
+		answer: 'Contact local Organizer.',
+	},
+	{
+		id: '3',
+		question: 'How to join to our meetup?',
+		answer: (
+			<>
+				Register to upcoming event on{' '}
+				<a
+					href="https://www.meetup.com/pl-PL/meet-js-lodz/"
+					className="font-bold hover:underline"
+					about="_blank"
+				>
+					meetup.com
+				</a>
+				. Join us on time in venue. It&apos;s easy 🥳
+			</>
+		),
+	},
+	{
+		id: '4',
+		question: 'How is it cost to join event?',
+		answer: (
+			<>
+				Generally our event are FREE! Only{' '}
+				<a href="https://summit.meetjs.pl" about="_blank">
+					summit
+				</a>{' '}
+				is paid event.
+			</>
+		),
+	},
+];
