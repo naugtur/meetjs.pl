aee3efa74547907baf6210f6f14a9f3f1139ca2c
Stanisław Synowiec <stasyn1410@gmail.com>
2024-11-15 13:02:59 +0100

    add: Aleksandra to Wrocław organizers

--- src/app/(cities)/wroclaw/_Organizers.tsx
+++ src/app/(cities)/wroclaw/_Organizers.tsx
@@ -11,6 +11,7 @@ export const WroclawOrganizers: Organizer[] = [
 		name: 'Aleksandra Pawlus',
 		image:
 			'https://media.licdn.com/dms/image/v2/D4D03AQE6uTN1xIKaqg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719568267585?e=1733961600&v=beta&t=MDkJF7RN575Jt3LagGeg8fUPRzXrwbt4yPfZ3UQuIDc',
+		linkedin: 'https://www.linkedin.com/in/aleksandrapawlus/',
 	},
 	{
 		name: 'Stanisław Synowiec',
