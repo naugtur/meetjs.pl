a0f5d4467e42d12bc250fd73e8e93470d3719fc2
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-01-08 17:54:59 +0100

    feat: create RankingBanner that would annuce that meet.js is nr 1 meetup in poland based on crossweb

---
+++ src/components/RankingBanner.tsx
@@ -0,0 +1,17 @@
+interface RankingBannerProps {
+    href: string;
+    text: string;
+}
+
+export const RankingBanner = ({ href, text }: RankingBannerProps) => {
+    return (
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute right-4 top-4 rotate-2 bg-blue px-4 py-2 shadow-lg transition-transform hover:scale-105"
+        >
+            <p className="text-sm font-bold">{text}</p>
+        </a>
+    );
+}; 
