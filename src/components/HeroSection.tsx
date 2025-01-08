e9b2d29e8f16efdc4c217a2f3e6f4ab7daf09dcf
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-01-08 17:54:59 +0100

    feat: create RankingBanner that would annuce that meet.js is nr 1 meetup in poland based on crossweb

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,17 @@
 import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
+import { RankingBanner } from '@/components/RankingBanner';
 
 export const HeroSection = () => {
 	return (
-		<header className="grid h-96 bg-[url('/conference.jpg')] bg-cover text-center text-white md:h-[35rem]">
+		<header className="relative grid h-96 bg-[url('/conference.jpg')] bg-cover text-center text-white md:h-[35rem]">
 			<div className="col-start-1 row-start-1 h-full w-full bg-gray-800 bg-opacity-70"></div>
+
+			<RankingBanner
+				href="https://crossweb.pl/blog/ranking-najpopularniejsze-wydarzenia-podcasty-wideo-cykle-w-crossweb-w-2024/"
+				text="🏆 #1 Tech Event Series in Poland (Crossweb 2024)"
+			/>
+
 			<div className="col-start-1 row-start-1 mx-auto my-auto">
 				<p className="-rotate-2 bg-blue p-2 text-6xl font-bold">meet.js</p>
 				<h1 className="p-4 text-xl font-medium">
