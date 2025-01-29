352d5ae172ef6ade73589e6468e5b37aef7c98fe
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-01-29 13:48:30 +0100

    feat: revalidate past and upcoming events every hour

--- src/utils/getUpcomingEvents.ts
+++ src/utils/getUpcomingEvents.ts
@@ -4,7 +4,11 @@ import { changeCityName } from '@/utils/changeCityName';
 
 export const getUpcomingEvents = async () => {
 	try {
-		const upcomingEventsRes = await fetch(new URL(env.EVENTS_API_URL));
+		const upcomingEventsRes = await fetch(new URL(env.EVENTS_API_URL), {
+			next: {
+				revalidate: 3600, // Cache for 1 hour (in seconds)
+			},
+		});
 
 		const upcomingEventsJson = await upcomingEventsRes.json();
 		const data = EventsSchema.parse(upcomingEventsJson);
