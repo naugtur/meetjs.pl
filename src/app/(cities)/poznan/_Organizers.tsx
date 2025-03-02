fa2fbb532933def87c257728639956d107c14500
Olaf Krawczyk <olaf.r.krawczyk@gmail.com>
2025-03-02 10:32:02 +0100

    use static photos

--- src/app/(cities)/poznan/_Organizers.tsx
+++ src/app/(cities)/poznan/_Organizers.tsx
@@ -3,25 +3,21 @@ import { Organizer } from '@/components/Orginizers';
 export const PoznanOrganizers: Organizer[] = [
 	{
 		name: 'Agnieszka Konefał',
-		image:
-			'https://media.licdn.com/dms/image/v2/C4D03AQFZr3qLKT0uVw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1644506046633?e=1734566400&v=beta&t=60-LKWQ1yb0m_WFrbrZt8lHvcQzgINekClvXjTGyWmw',
+		image: '/city/poznan/organizers/agnieszka.jpg',
 		linkedin: 'https://www.linkedin.com/in/agnieszka-konefał-9a731bb2',
 	},
 	{
 		name: 'Sebastian Pożoga',
-		image:
-			'https://media.licdn.com/dms/image/v2/C4D03AQFUKKPQpW03Tg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1516522084435?e=1734566400&v=beta&t=Etzlth5uLxdUL0q3AILLA6y4BRBpWog5g2jSUknHchs',
+		image: '/city/poznan/organizers/sebastian.jpeg',
 	},
 	{
 		name: 'Marcin Owczarzak',
 		linkedin: 'https://www.linkedin.com/in/marcinowczarzak/',
-		image:
-			'https://media.licdn.com/dms/image/v2/C4E03AQF55igdl9cZWg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1614360129676?e=1734566400&v=beta&t=0TVIruOrCai_DqZyh0lvhtwUonamSuywbnhaUiF2d-g',
+		image: '/city/poznan/organizers/marcin.jpeg',
 	},
 	{
 		name: 'Olaf Krawczyk',
-		image:
-			'https://media.licdn.com/dms/image/v2/D4D03AQFz9w4gS_Li7w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1696614708912?e=1734566400&v=beta&t=0ogT5E21S8nkK8NoMBSTA0nyavmjTrIZ77m3RPbpv4Q',
+		image: '/city/poznan/organizers/olaf.jpeg',
 		linkedin: 'https://www.linkedin.com/in/olaf-krawczyk/',
 	},
 ];
