cdf7f51ea94898d4f7e4bccdf29e6b09794e4588
Michał Korczak <michal.k2001@gmail.com>
2025-03-22 21:21:43 +0000

    fix: my friend

--- src/app/(cities)/wroclaw/_Organizers.tsx
+++ src/app/(cities)/wroclaw/_Organizers.tsx
@@ -3,8 +3,7 @@ import { Organizer } from '@/components/Orginizers';
 export const WroclawOrganizers: Organizer[] = [
 	{
 		name: 'Cezary Dynak',
-		image:
-			'https://media.licdn.com/dms/image/v2/C5603AQEG4Z0uFmofJg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1554878839563?e=1733961600&v=beta&t=LNTwIpmkSlWRAB-F-e8hNTbrk5IlmKBm6epUwyyNjd0',
+		image: 'https://avatars.githubusercontent.com/u/12508532?v=4',
 		linkedin: 'https://www.linkedin.com/in/cdynak/',
 	},
 	{
