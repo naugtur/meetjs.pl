5858e95297850799ade12670cb719ac1fa3feec7
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-03-23 10:58:45 +0100

    feat: remove breadcrumb

--- src/components/CityBanner.tsx
+++ src/components/CityBanner.tsx
@@ -1,12 +1,3 @@
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-
 interface CityBannerProps {
 	city: string;
 	background: string;
@@ -18,22 +9,7 @@ export const CityBanner = ({ city, background }: CityBannerProps) => {
 			className="flex flex-col items-center justify-center bg-cover bg-center p-28"
 			style={{ backgroundImage: `url(${background})` }}
 		>
-			<h1 className="text-6xl font-bold text-white">{city}</h1>
-			<Breadcrumb className="bg-gray-400/50 p-2">
-				<BreadcrumbList>
-					<BreadcrumbItem>
-						<BreadcrumbLink href="/" className="text-white">
-							Home
-						</BreadcrumbLink>
-					</BreadcrumbItem>
-					<BreadcrumbSeparator className="text-white" />
-					<BreadcrumbItem>
-						<BreadcrumbPage className="font-bold text-white">
-							{city}
-						</BreadcrumbPage>
-					</BreadcrumbItem>
-				</BreadcrumbList>
-			</Breadcrumb>
+			<h1 className="text-6xl font-bold text-white drop-shadow-lg">{city}</h1>
 		</section>
 	);
 };
