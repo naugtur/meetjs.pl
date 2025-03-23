082c7475bee35eaddce13ead70ed27a58e1c69e3
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-03-23 11:45:22 +0100

    feat: add Lublin page

---
+++ src/app/(cities)/lublin/_FAQ.tsx
@@ -0,0 +1,59 @@
+import { FAQQuestion } from '@/components/FAQ';
+
+export const LublinFAQ: FAQQuestion[] = [
+	{
+		id: '4',
+		question: 'How to become Lublin meetup sponsor?',
+		answer: `Email organizers and they'll  get back to you with details. You can host us at your office or help us cover the cost of a venue. Or we can organize the meetup outdoors :) We're open to ideas!`,
+	},
+	{
+		id: '2',
+		question: 'How to become Lublin speaker?',
+		answer: (
+			<>
+				Go to{' '}
+				<a
+					href="https://github.com/meetjspl/lublin/issues/"
+					className="font-bold hover:underline"
+					about="_blank"
+				>
+					https://github.com/meetjspl/lublin/issues/
+				</a>
+				{' '}and suggest a topic
+			</>
+		),
+
+	},
+	{
+		id: '3',
+		question: 'How to join to our meetup?',
+		answer: (
+			<>
+				Register to upcoming event on{' '}
+				<a
+					href="https://www.meetup.com/pl-PL/meetjslublin/"
+					className="font-bold hover:underline"
+					about="_blank"
+				>
+					meetup.com
+				</a>
+				. Join us on time in venue. It&apos;s easy 🥳
+			</>
+		),
+	},
+	{
+		id: '1',
+		question: 'What does it cost to join event?',
+		answer: (
+			<>
+				Generally our event are FREE! Only{' '}
+				<a href="https://summit.meetjs.pl" about="_blank">
+					summit
+				</a>{' '}
+				is paid event.
+				<br />
+				We sometimes organize charity events where you can donate to participate.
+			</>
+		),
+	},
+];
---
+++ src/app/(cities)/lublin/_Organizers.tsx
@@ -0,0 +1,9 @@
+import { Organizer } from '@/components/Orginizers';
+
+export const LublinOrganizers: Organizer[] = [
+	{
+		name: 'Patryk Omiotek',
+		image: 'https://media.licdn.com/dms/image/v2/D4D03AQEE4b1f0BdSWA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726523493093?e=1748476800&v=beta&t=xuOpEwgpOalNS10CWw9v8tLPmOdc5INWvzttkMhXQ-M',
+		linkedin: 'https://www.linkedin.com/in/patrykomiotek/',
+	}
+];
