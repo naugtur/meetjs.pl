e809f4c1a0dcf032d2a900bd7cdef847d31ba358
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-03-26 03:34:01 +0100

    Add TODO for variants

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,6 +2,7 @@ interface Props {
 	children: React.ReactNode;
 }
 
+// TODO: Introduce variants - https://github.com/naugtur/meetjs.pl/issues/129
 export const Banner = ({ children }: Props) => (
 	<article className="mb-8 border-l-4 border-yellow-400 bg-yellow-50 p-4">
 		{children}
