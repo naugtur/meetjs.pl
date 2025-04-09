c98292f63b7c2b76d5f2d293eb3330834be67ae2
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-04-09 11:33:44 +0200

    Add Banner variants

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,35 @@
+import { classNames } from '@/utils/classNames';
+
+type Variant = 'info' | 'success' | 'warning' | 'danger';
+
 interface Props {
 	children: React.ReactNode;
+	variant?: Variant;
 }
 
 // TODO: Introduce variants - https://github.com/naugtur/meetjs.pl/issues/129
-export const Banner = ({ children }: Props) => (
-	<article className="mb-8 border-l-4 border-yellow-400 bg-yellow-50 p-4">
+export const Banner = ({ children, variant = 'info' }: Props) => (
+	<article
+		className={classNames(
+			'mb-8 border-l-4 p-4 shadow-sm',
+			getVariantStyles(variant),
+		)}
+	>
 		{children}
 	</article>
 );
+
+const getVariantStyles = (variant: Variant) => {
+	switch (variant) {
+		case 'info':
+			return 'bg-blue-50 border-blue-400';
+		case 'success':
+			return 'bg-green-50 border-green-400';
+		case 'warning':
+			return 'bg-yellow-50 border-yellow-400';
+		case 'danger':
+			return 'bg-red-50 border-red-400';
+		default:
+			throw new Error(`Unknown Banner variant of "${variant}"`);
+	}
+};
