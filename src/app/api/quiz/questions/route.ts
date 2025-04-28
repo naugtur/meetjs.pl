cc22f31cee4c43e967c4020a303854767fe1e012
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-04-28 10:37:21 +0200

    build fix

--- src/app/api/quiz/questions/route.ts
+++ src/app/api/quiz/questions/route.ts
@@ -2,4 +2,4 @@ import questions from '@/data/quiz/questions.json';
 
 // Todo: Create question presets/lists. Add random question.
 // - Question search (text search, category, difficulty, filter, pagination)
-export const GET = async (request: Request) => Response.json(questions);
+export const GET = async () => Response.json(questions);
