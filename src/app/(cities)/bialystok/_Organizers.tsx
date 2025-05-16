f9d0367ea9908a4dd496e0cbbefc530d4981eb2e
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-05-16 12:18:52 +0200

    refactor: move Organizer type to dedicated types directory

--- src/app/(cities)/bialystok/_Organizers.tsx
+++ src/app/(cities)/bialystok/_Organizers.tsx
@@ -1,4 +1,4 @@
-import { Organizer } from '@/components/Orginizers';
+import { Organizer } from '@/types/organizer';
 
 export const BialystokOrganizers: Organizer[] = [
 	{
@@ -17,13 +17,12 @@ export const BialystokOrganizers: Organizer[] = [
 		name: 'Krzysztof Kondracikowski',
 		image:
 			'https://media.licdn.com/dms/image/v2/C5603AQFv3ht2Mpp3zQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1662368294098?e=1737590400&v=beta&t=wYIudvUPW3nz7BA_6KxN68eJDA2JK9t1Ym7jkzRUSes',
-            linkedin: `https://www.linkedin.com/in/krzysztof-kondracikowski-87a3491b3/`,
+		linkedin: `https://www.linkedin.com/in/krzysztof-kondracikowski-87a3491b3/`,
 	},
-    {
+	{
 		name: 'Mateusz Butkiewicz',
 		image:
 			'https://media.licdn.com/dms/image/v2/C4E03AQEvThc2J3-nvQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1642884417355?e=1737590400&v=beta&t=vjl0p0x8WNJblNWf8p4P-HQwJO3Cp0oyrUuqL-oqY28',
 		linkedin: 'https://www.linkedin.com/in/mat-btk/',
 	},
-	
 ];
--- src/app/(cities)/katowice/_Organizers.tsx
+++ src/app/(cities)/katowice/_Organizers.tsx
@@ -1,9 +1,10 @@
-import { Organizer } from '@/components/Orginizers';
+import { Organizer } from '@/types/organizer';
 
 export const KatowiceOrganizers: Organizer[] = [
 	{
 		name: 'Sebastian Mysakowski',
-		image: 'https://media.licdn.com/dms/image/v2/D4D03AQHJqYudYW7bdg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1663656658788?e=1748476800&v=beta&t=Q_Sh2y_ZvPOcSfLX2rogHlScXwKaA9VC6A9yEnXUHa8',
+		image:
+			'https://media.licdn.com/dms/image/v2/D4D03AQHJqYudYW7bdg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1663656658788?e=1748476800&v=beta&t=Q_Sh2y_ZvPOcSfLX2rogHlScXwKaA9VC6A9yEnXUHa8',
 		linkedin: 'https://www.linkedin.com/in/smysakowski/',
-	}
+	},
 ];
