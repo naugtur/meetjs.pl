077447605705956f59c8e66fda78207181f408e7
Robert Harężlak <harezlak.robert7@gmail.com>
2025-07-16 13:35:44 +0200

    feat: update avatars

--- src/app/(cities)/bielsko-biala/_Organizers.tsx
+++ src/app/(cities)/bielsko-biala/_Organizers.tsx
@@ -3,19 +3,19 @@ import { Organizer } from '@/types/organizer';
 export const BielskoBialaOrganizers: Organizer[] = [
   {
     name: 'Dariusz Wylon',
-    image: 'https://avatars.githubusercontent.com/u/73687634?v=4',
+    image: 'https://media.licdn.com/dms/image/v2/D4D03AQGPcHctay9tpg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1708636222708?e=1758153600&v=beta&t=Rna3uIShDqfqsVF3m1khnPut01p2agFzahh0EDDgX9Q',
     email: 'd.wylon@selleo.com',
     linkedin: 'https://www.linkedin.com/in/dariuszwylon/',
   },
   {
     name: 'Dawid Poślinski',
-    image: 'https://avatars.githubusercontent.com/u/1891508?v=4',
+    image: 'https://media.licdn.com/dms/image/v2/D4D03AQG4Ev-f9q5gNA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1708187543319?e=1758153600&v=beta&t=YBe-IFfM4EpqAP4X8itFjbt72Tm4mTdL8sZ6ySd88n0',
     email: 'd.poslinski@selleo.com',
     linkedin: 'https://www.linkedin.com/in/dawid-poslinski/',
   },
   {
     name: 'Maria Nowicka-Pawlik',
-    image: '',
+    image: 'https://media.licdn.com/dms/image/v2/D4D03AQHhbeH0NEeHEQ/profile-displayphoto-shrink_200_200/B4DZaAxJOVGgAY-/0/1745917089055?e=1758153600&v=beta&t=igrRFOAf_gdBMDT_VWzEwF1kos8PzuVJyLWqWzEHiMk',
     email: 'm.nowickapawlik@selleo.com',
     linkedin: 'https://www.linkedin.com/in/maria-nowicka-pawlik/',
   },
