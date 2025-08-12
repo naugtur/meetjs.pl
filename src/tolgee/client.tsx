4a0990c7459e985efa0af33bcb48e1390530d746
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-08-12 08:51:58 +0200

    feat: implement Tolgee internationalization with English and Polish translations

---
+++ src/tolgee/client.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { TolgeeBase } from './shared';
+import {
+  CachePublicRecord,
+  TolgeeProvider,
+  TolgeeStaticData,
+} from '@tolgee/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+type Props = {
+  language: string;
+  staticData: TolgeeStaticData | CachePublicRecord[];
+  children: React.ReactNode;
+};
+
+const tolgee = TolgeeBase().init();
+
+export const TolgeeNextProvider = ({
+  language,
+  staticData,
+  children,
+}: Props) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // this ensures server components refresh, after translation change
+    const { unsubscribe } = tolgee.on('permanentChange', () => {
+      router.refresh();
+    });
+    return () => unsubscribe();
+  }, [tolgee, router]);
+
+  return (
+    <TolgeeProvider
+      tolgee={tolgee}
+      fallback="Loading..."
+      ssr={{ language, staticData }}
+    >
+      {children}
+    </TolgeeProvider>
+  );
+};
