a6af81b4ec3f97de3e51a5de63f510f0fb116e96
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-08-12 09:38:10 +0200

    feat: add translations for organizer page and make it dynamic

--- src/app/how-to-become-an-organizer/layout.tsx
+++ src/app/how-to-become-an-organizer/layout.tsx
@@ -1,24 +1,29 @@
 import { Metadata } from 'next';
+import { getTranslate } from '@/tolgee/server';
 
-export const metadata: Metadata = {
-  title: 'How to become an organizer | meet.js',
-  description: 'How to become an organizer for meet.js.',
-  openGraph: {
-    title: 'How to become an organizer | meet.js',
-    description: 'How to become an organizer for meet.js.',
-    url: 'https://meetjs.pl/how-to-become-an-organizer',
-    siteName: 'meet.js',
-    images: [
-      {
-        url: 'https://meetjs.pl/og-image.png',
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslate();
+  
+  return {
+    title: t('organizer.meta_title'),
+    description: t('organizer.meta_description'),
+    openGraph: {
+      title: t('organizer.meta_title'),
+      description: t('organizer.meta_description'),
+      url: 'https://meetjs.pl/how-to-become-an-organizer',
+      siteName: 'meet.js',
+      images: [
+        {
+          url: 'https://meetjs.pl/og-image.png',
+          width: 1200,
+          height: 630,
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+  };
+}
 
 export default function HowToBecomeAnOrganizerLayout({
   children,
