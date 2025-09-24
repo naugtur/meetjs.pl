0d66ccb42cc5cae04b02e7d193b1af1172973036
Stanisław Synowiec <stasyn1410@gmail.com>
2025-09-24 11:51:43 +0200

    chore(env): use env const

--- src/env.ts
+++ src/env.ts
@@ -5,9 +5,11 @@ export const env = createEnv({
   server: {
     EVENTS_API_URL: z.string().url(),
     SITE_URL: z.string().url(),
+    DISCORD_SERVER_ID: z.string(),
   },
   runtimeEnv: {
     EVENTS_API_URL: process.env.EVENTS_API_URL,
     SITE_URL: process.env.SITE_URL,
+    DISCORD_SERVER_ID: process.env.DISCORD_SERVER_ID,
   },
 });
